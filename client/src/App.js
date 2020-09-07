a6c3a5aceb813315fee832043fddffddf5705682
kjx63 <ueyama.kenji.63@gmail.com>
2020-09-07 23:07:52 +0900

    Build Navbar Component with reactstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import AppNavbar from './components/AppNavbar';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Shopping List</h1>
+      <AppNavbar />
     </div>
   );
 }
