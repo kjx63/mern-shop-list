b75efe8522f2a22672bd217205dc2677e9a7e962
kjx63 <ueyama.kenji.63@gmail.com>
2020-09-08 12:20:43 +0900

    Connect the backend / getItems from the mongodb

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,13 +1,14 @@
 import { uuid } from 'uuidv4';
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+import {
+  GET_ITEMS,
+  ADD_ITEM,
+  DELETE_ITEM,
+  ITEMS_LOADING,
+} from '../actions/types';
 
 const initialState = {
-  items: [
-    { id: uuid(), name: 'Eggs' },
-    { id: uuid(), name: 'Milk' },
-    { id: uuid(), name: 'Steak' },
-    { id: uuid(), name: 'Water' },
-  ],
+  items: [],
+  loading: false,
 };
 
 export default function (state = initialState, action) {
@@ -15,6 +16,8 @@ export default function (state = initialState, action) {
     case GET_ITEMS:
       return {
         ...state,
+        items: action.payload,
+        loading: false,
       };
     case DELETE_ITEM:
       return {
@@ -25,6 +28,11 @@ export default function (state = initialState, action) {
       return {
         ...state,
         items: [action.payload, ...state.items],
+      };
+    case ITEMS_LOADING:
+      return {
+        ...state,
+        loading: true,
       };
     default:
       return state;
